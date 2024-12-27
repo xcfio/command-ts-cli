a24d0621a4719f813db3f514d2d94abe4966a4c9
xcfio <119097812+xcfio@users.noreply.github.com>
2024-12-27 11:24:26 +0600

    Add cmd-ts-cli command and colorette dependency; update CLI versioning

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 
 import { program } from "commander"
+import { version } from "../package.json"
 
 program
-    .version("0.0.1")
+    .version(version)
     .description("Command line interface for the command-ts library")
     .action((options) => {
         console.log(`This is the command-ts CLI version ${program.version()}`)
---
+++ src/note.ts
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+
+import { blue, italic } from "colorette"
+process.emitWarning(blue(`${italic("cmd-ts-cli")} isn’t what you’re looking for. Try ${italic("ctscli")} instead`))
